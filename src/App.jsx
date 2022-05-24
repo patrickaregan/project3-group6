e8b112f8d80481af928d04b01bcc29b9dc8dac6f
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-23 20:24:41 -0500

    created Login and Signup jsx and scss file. Tranformed App.css to App.scss and App.js to App.jsx. Editted the App.jsx file and imported the Login and Signup Jsx files.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+     <Signup/>
+    </div>
+  );
+}
+
+export default App;
