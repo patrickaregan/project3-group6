582f01d212c28c32f8061c201b5164a9f7e0f315
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-25 19:06:10 -0500

    set up very basic outline for navbar and login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,12 @@ import "./login.scss";
 
 export default function Login() {
   return (
-      <div class="">
-          Login
+    <div class="Login">
+      <div class="Hero"></div>
+      <div class="Container">
+        <div class="left"></div>
+        <div class="right"></div>
       </div>
+    </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import './navbar.scss';
+import "./navbar.scss";
 
 export default function Navbar() {
   return (
-    <div>Navbar
-        <div>
+    <div class="navbar">
+      <div class="container">
+        <div class="left">
+          {/* font awesome icon for logo */}
+          {/* Browse, community, and searchbar */}
         </div>
+        <div class="right">
+          {/* hamburger menu */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
