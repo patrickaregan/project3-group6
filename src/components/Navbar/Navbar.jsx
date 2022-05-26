911fb6a7cb111cfa96269ba8bd7787c5166c7381
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-25 19:28:43 -0500

    added fontawesome icons. added the book icon and styled it a little

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import "./navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
@@ -6,6 +8,7 @@ export default function Navbar() {
       <div class="container">
         <div class="left">
           {/* font awesome icon for logo */}
+          <FontAwesomeIcon icon={ faBookOpen } class="icon" />
           {/* Browse, community, and searchbar */}
         </div>
         <div class="right">
