28842b9f5aed0defaceead4b95d04656ffc4abe6
patrickaregan <patrickaregan@gmail.com>
2022-05-25 19:03:11 -0500

    create models for User, Story and Line

---
+++ server/models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
+    },
+    stories: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Story'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('storyCount').get(function() {
+  return this.stories.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
