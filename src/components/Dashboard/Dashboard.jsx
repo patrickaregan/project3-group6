c40293c790a6ef7128d58030f4db27a54f3468e7
Kaleb Stafford <kalebstafford0@gmail.com>
2022-05-26 21:47:18 -0500

    carousel display

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,20 +2,35 @@ import "./dashboard.scss";
 
 export default function Dashboard() {
   return (
-    <div class="dashboardMain">
-      <div class="hero-container"></div>
-      <div class="scrolling-container">
-        <div class="scrollHeading">
+    <div className="dashboardMain">
+      <div className="hero-container"></div>
+      <div className="scrolling-container">
+        <div className="scrollHeading">
           <h2>Your Stories</h2>
         </div>
-        <div
-          class="bookContainer"
-        >
-          <div class="bookCell"></div>
-          <div class="bookCell"></div>
-          <div class="bookCell"></div>
-          <div class="bookCell"></div>
-          <div class="bookCell"></div>
+        <div className="bookMain">
+          <input type="radio" name="position" defaultChecked />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <input type="radio" name="position" />
+          <div className="bookContainer">
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+            <div className="bookCell"></div>
+          </div>
         </div>
       </div>
     </div>
