53a71894df8ad05b5fd82db04927b4c67ea8fe0d
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-31 17:23:42 -0500

    finished signup (for now). ixed a few things

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,15 +3,29 @@ import "./login.scss";
 export default function Login() {
   return (
     <div class="Login">
-      <div class="Hero"/>
+      <div class="Hero" />
       <div class="Container">
         <div class="left">
           <div class="loginContainer">
             <h2>Log In</h2>
-            <input type="text" class="username" placeholder="Username" id="username"/>
-            <input type="text" class="password" placeholder="Password" id="password"/>
-            <button type="button" id="loginButton">Log In</button>
-            <h4>Or sign up <a href="signup">here</a></h4>
+            <input
+              type="text"
+              class="username"
+              placeholder="Username"
+              id="username"
+            />
+            <input
+              type="text"
+              class="password"
+              placeholder="Password"
+              id="password"
+            />
+            <button type="button" id="loginButton">
+              Log In
+            </button>
+            <h4>
+              Or sign up <a href="signup">here</a>
+            </h4>
           </div>
         </div>
         <div class="right">
@@ -19,5 +33,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-  )
+  );
 }
