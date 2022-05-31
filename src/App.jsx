c8e1310e5106a8885eb4954a8dccfaaa4357d957
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-31 15:36:56 -0500

    a ton of touchups plus routes to other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
-// import Signup from "./components/Signup/Signup";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Login />
-      {/* <Signup /> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
